9ffe6dabf5c5bff776e5c039aec2c9173f6c6d38
Hussein Omar <husseinomar6190@gmail.com>
2022-04-17 19:59:40 +0300

    adding constructor paramiters to classes

--- src/app/repos.ts
+++ src/app/repos.ts
@@ -1,2 +1,8 @@
 export class Repos {
+  name: string;
+  data: object;
+  constructor(public userName: string, public dataReceived: object) {
+    this.name = userName;
+    this.data = dataReceived;
+  }
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,8 @@
 export class User {
+  name: string;
+  data: object;
+  constructor(public userName: string, public dataReceived: object) {
+    this.name = userName;
+    this.data = dataReceived;
+  }
 }
