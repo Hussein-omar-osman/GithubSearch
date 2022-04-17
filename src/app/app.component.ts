0ce977d56573e19dc316f7e2ef0397ad57ae9af2
Hussein Omar <husseinomar6190@gmail.com>
2022-04-17 15:10:28 +0300

    adding routing module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+// import { GitHubApiService } from './git-hub-api.service';
+// import { GithubUser } from './github-user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'githubSearch';
+  // constructor(private gitHubApiService: GitHubApiService) {}
+  // sendSearch(data: string) {
+  //   this.gitHubApiService.getSearchTerm(data);
+  // }
 }
