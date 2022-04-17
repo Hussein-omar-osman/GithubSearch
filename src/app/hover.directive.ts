a04df047d67a03d18a3290f23dcb385f59056378
Hussein Omar <husseinomar6190@gmail.com>
2022-04-17 19:33:49 +0300

    implementing hover directive inside github user component

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[appHover]',
@@ -7,4 +13,21 @@ export class HoverDirective implements OnInit {
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {}
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.renderer.addClass(this.element.nativeElement, 'hover');
+    this.renderer.setStyle(
+      this.element.nativeElement,
+      'transition',
+      'all 0.3s ease-in-out;'
+    );
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.renderer.removeClass(this.element.nativeElement, 'hover');
+    this.renderer.setStyle(
+      this.element.nativeElement,
+      'transition',
+      'all 0.3s ease-in-out;'
+    );
+  }
 }
