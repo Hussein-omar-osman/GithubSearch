2a235e6ed8b1f03f59c129929f3ff1925242b7a6
Hussein Omar <husseinomar6190@gmail.com>
2022-04-17 15:35:41 +0300

    creating routing path for github repos component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GihubReposComponent } from './gihub-repos/gihub-repos.component';
 import { GithubUserComponent } from './github-user/github-user.component';
 
 const routes: Routes = [
   {
     path: 'githubUser',
     component: GithubUserComponent,
+  },
+  {
+    path: 'githubRepos',
+    component: GihubReposComponent,
   },
 ];
 @NgModule({
