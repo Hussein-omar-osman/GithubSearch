509754fd6ede6bc175991ecb908ba071223ec155
Hussein Omar <husseinomar6190@gmail.com>
2022-04-17 15:22:17 +0300

    adding the http get request to service

--- src/app/git-hub-api.service.ts
+++ src/app/git-hub-api.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GitHubApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // userName;
+  // getSearchTerm(name: string) {
+  //   this.userName = name;
+  //   console.log(this.userName);
+  // }
+
+  getGithubUser(search) {
+    console.log(search);
+
+    return this.http.get(`https://api.github.com/users/${search}`);
+  }
+  getGithubUserRepos(search) {
+    console.log(search);
+
+    return this.http.get(`https://api.github.com/users/${search}/repos`);
+  }
 }
