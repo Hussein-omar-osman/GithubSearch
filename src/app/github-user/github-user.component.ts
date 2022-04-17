5e47ea56ba5ff2f53e681ee3704339afd2737450
Hussein Omar <husseinomar6190@gmail.com>
2022-04-17 15:23:52 +0300

    making the submit function and requesting data from api

--- src/app/github-user/github-user.component.ts
+++ src/app/github-user/github-user.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { GitHubApiService } from '../git-hub-api.service';
 
 @Component({
   selector: 'app-github-user',
   templateUrl: './github-user.component.html',
-  styleUrls: ['./github-user.component.css']
+  styleUrls: ['./github-user.component.css'],
 })
 export class GithubUserComponent implements OnInit {
+  constructor(private gitHubApiService: GitHubApiService) {}
 
-  constructor() { }
+  user;
+  repos;
+  reposFetched = false;
+  dataFetched = false;
+  searchTerm: string;
+  onsubmit(form: NgForm) {
+    this.searchTerm = form.value.userSearch;
+    console.log(this.searchTerm);
 
-  ngOnInit(): void {
+    this.gitHubApiService.getGithubUser(this.searchTerm).subscribe((data) => {
+      this.user = data;
+      this.dataFetched = true;
+      console.log(data);
+    });
+
+    this.gitHubApiService
+      .getGithubUserRepos(this.searchTerm)
+      .subscribe((data) => {
+        this.repos = data;
+        this.reposFetched = true;
+        console.log(data);
+      });
+
+    form.reset();
   }
-
+  ngOnInit() {
+    // let user = new GithubUser();
+    // this.user = user.userData;
+    // console.log(this.user.login);
+  }
 }
